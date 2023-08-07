2b0196072549c908d252326a80166e11a5b54519
Rubén Prieto Serrano <131255999+Rubpri@users.noreply.github.com>
2023-08-07 19:21:08 +0000

    version 2

--- src/models.py
+++ src/models.py
@@ -74,6 +74,46 @@ class Favorites(Base):
     starships_id = Column(Integer, ForeignKey('starships.id'))
     starships = relationship(Starships)
 
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20)) 
+    firstname = Column(String(20)) 
+    lastname = Column(String(20)) 
+    email = Column(String(50))
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(String(20), ForeignKey('user.id'))
+    user = relationship(User)
+
+class Media(Base):
+    __tablename__ = 'media'
+    id = Column(Integer, primary_key=True)
+    type = Column(String(20))
+    url = Column(String(250))
+    post_id= Column(String(20), ForeignKey('post.id'))
+    post = relationship(Post)
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    comment_text = Column(String(20))
+    post_id= Column(String(20), ForeignKey('post.id'))
+    post = relationship(Post)
+    author_id= Column(String(20), ForeignKey('user.id'))
+    author = relationship(User)
+    
+class Follower(Base):
+    __tablename__ = 'follower'
+    id = Column(Integer, primary_key=True)
+    user_from_id= Column(String(20), ForeignKey('user.id'))
+    user_from = relationship(User)
+    user_to_id= Column(String(20), ForeignKey('user.id'))
+    user_to = relationship(User)
+
+
 
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
